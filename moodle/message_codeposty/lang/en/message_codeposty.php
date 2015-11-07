bf593c376dfe486d992555b00271c3ac1d497c17
sytco <info@syt.co.ir>
2015-11-07 14:32:16 +0330

    Create message_codeposty.php

---
+++ moodle/message_codeposty/lang/en/message_codeposty.php
@@ -0,0 +1,33 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * codeposty message processor version information
+ *
+ * @package    message_codeposty
+ * @copyright  2015 codeposty.ir
+ * @license    http://www.codeposty.ir
+ */
+$string['configcodepostynumber'] = 'The sender number';
+$string['configcodepostyusername'] = 'The user name to use when connecting to the sms server';
+$string['configcodepostypassword'] = 'The password to use when connecting to the sms server';
+$string['codepostynumber'] = 'SMS Panel acount\'s number';
+$string['codepostyusername'] = 'SMS Panel account\'s user name';
+$string['codepostypassword'] = 'SMS Panel account\'s password';
+$string['notconfigured'] = 'The SMS server hasn\'t been configured so SMS messages cannot be sent';
+$string['codepostymobilenumber'] = 'Mobile number';
+$string['pluginname'] = 'Codeposty';
