a27b3c13e4d19bc40b7137c649dbc43cad14945a
sytco <info@syt.co.ir>
2015-11-07 14:33:01 +0330

    Create message_codeposty.php

---
+++ moodle/message_codeposty/lang/fa/message_codeposty.php
@@ -0,0 +1,34 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * codeposty message processor version information
+ *
+ * @package    message_codeposty
+ * @copyright  2015 codeposty.ir
+ * @license    http://www.codeposty.ir
+ */
+
+$string['configcodepostynumber'] = 'شماره ارسال کننده';
+$string['configcodepostyusername'] = 'نام کاربری پنل ارسال پیامک';
+$string['configcodepostypassword'] = 'رمز عبور پنل ارسال پیامک';
+$string['codepostynumber'] = 'شماره';
+$string['codepostyusername'] = 'نام کاربری';
+$string['codepostypassword'] = 'رمز عبور';
+$string['notconfigured'] = 'ماژول ارسال پیامک استاندارد هنوز پیکربندی نشده است';
+$string['codepostymobilenumber'] = 'شماره تلفن همراه';
+$string['pluginname'] = 'افزونه ارسال پیامک کدپستی (پنل پیامک: <a href="http://www.codeposty.ir" target="_blank" title="سامانه پیامک کدپستی">کدپستی</a>)';
